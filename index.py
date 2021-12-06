3f9bd6f62ed824c58b2b64b03f369cc8a8e60815
Alex Luo <yluo6@uw.edu>
2021-12-06 00:24:43 -0800

    Move ITAD handler outside of main program

--- index.py
+++ index.py
@@ -10,11 +10,17 @@ from flask import Flask
 rawHTML = urllib.request.urlopen('https://howlongtobeat.com/game?id=93948')
 doc = rawHTML.read().decode('utf8')
 soup:BeautifulSoup = BeautifulSoup(doc, 'html.parser')
-print(soup.prettify())
 
 playtime = soup.findAll("li", class_="short time_100")
-print(playtime)
+# print(playtime)
+
+# rawHTML = urllib.request.urlopen('https://store.steampowered.com/search/?filter=topsellers')
+# doc = rawHTML.read().decode('utf8')
+# soup:BeautifulSoup = BeautifulSoup(doc, 'html.parser')
+# top_games = soup.findAll("span", class_="title")
+# print(top_games)
 exit()
 
 # Main method
 if __name__ == "__main__":
+    exit()
