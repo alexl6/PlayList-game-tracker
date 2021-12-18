3ff5065b9cffc397cc579e429aa3984c306a27f1
Alex <62918876+alexl6@users.noreply.github.com>
2021-12-18 15:53:16 -0800

    Update README.md

---
+++ keys.py
@@ -0,0 +1,7 @@
+# Locally stores the API key, client ID & secrets
+ITAD_key = 'e5196f562f3644b0e4b22b369a4091986b07cdc1'
+IGDB_ID = 'kotsmb199kn43b03p1jbt8ou92h9s3'
+IGDB_secret = 'u5l61u6e83fmz8gd2lvwmz5a9aqc9v'
+
+# Cached credentials for testing
+IGDB_token = "0y8fj1a7l0heefyny8irs3f3y1gtpi"
